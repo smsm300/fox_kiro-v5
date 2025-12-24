76ffd2d0cab01e7734cfc8c6686a07bd23d7afad
skn2lrn <skn2lrn@gmail.com>
2025-12-24 17:27:46 +0200

    delete

--- fox-group-erp/handlers/index.ts
+++
@@ -1,5 +0,0 @@
-// This file will be created to contain all handlers
-// For now, we'll keep them in App.tsx but this is a placeholder
-// for future refactoring when we extract handlers
-
-export {};
--- fox-group-erp/hooks/useAppState.ts
+++
@@ -1,7 +0,0 @@
-// This hook is deprecated - we now load data directly from API in each component
-// Keeping this file for backward compatibility but it's no longer used
-
-export const useAppState = () => {
-  console.warn('useAppState is deprecated - load data from API instead');
-  return null;
-};
